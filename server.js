a0e1b38c42b5b5afab4ff993b108abe5302ab86e
gib <gibbowden@gmail.com>
2017-12-03 23:39:43 -0600

    basic setup done, working on heroku deployment

---
+++ server.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const express = require('express');
+const app = express();
+const request = require('request');
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
